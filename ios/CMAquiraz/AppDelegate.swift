a6226d00f7eeb865c2e06278546ec78dc6affee6
tecnologiaeudesenvolvo <tecnologia@eudesenvolvo.com>
2025-11-27 11:00:19 -0300

    pre-build

--- ios/CMAquiraz/AppDelegate.swift
+++ ios/CMAquiraz/AppDelegate.swift
@@ -1,7 +1,6 @@
 import Expo
 import React
 import ReactAppDependencyProvider
-import Firebase
 
 @UIApplicationMain
 public class AppDelegate: ExpoAppDelegate {
@@ -29,8 +28,6 @@ public class AppDelegate: ExpoAppDelegate {
       in: window,
       launchOptions: launchOptions)
 #endif
-
-    FirebaseApp.configure()
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
